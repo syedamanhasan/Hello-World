12f85df63db32b61203ebe3b008a4abaffb20d53
syedamanhasan <91936144+syedamanhasan@users.noreply.github.com>
2023-07-13 04:59:32 -0700

    Add files via upload
    My first Program

---
+++ basic.java
@@ -0,0 +1,22 @@
+package JAVA;
+import java.util.*;
+public class basic{
+
+    public static void main(Strings[] args){
+
+        System.out.println("Hello");
+        int a =10;
+        int b =20;
+        int sum=a+b;
+
+        System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+        int d= sc.nextInt();
+        int e = sc.nextInt();
+        int add=d+e;
+        System.out.println(add);
+
+
+    }
+}
